30e292d61229f922f5c6597cad6d5b180bc87113
mohamadAlbzoor <m.z.bzoor@gmail.com>
2021-07-18 15:20:55 +0300

    front almost done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import CardUI from "./components/CardUI"
+import data from "./services/test-service"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="navbar justify-content-center">
+        <input
+            type="text"
+            id="header-search"
+            placeholder="   SEARCH BOOKS"
+            name="s"
+            className="input"
+        />
+        <button type="submit" className="btn btn1 btn-outline-success" >Search</button>
+      </div>
+      <div className="container-fluid d-flex justify-content-between">
+          <div className="row">
+              {data.cardData.map((item, index) => {
+                  return(
+                      <div className="col-md-3">
+                          <CardUI key={index} title={item.title} index={index}/>
+                      </div>
+                  )
+              })}
+
+          </div>
+      </div>
     </div>
   );
 }
